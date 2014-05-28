ba402038f4c0b36d3b5749cb35db7d7b809d22eb
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-28 09:15:38 -0500

    go why must you do this to me

--- pipeline/commit.sh
+++ pipeline/commit.sh
@@ -4,6 +4,8 @@ echo 0
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 echo 1
 rvm install ruby-1.9.3-p484
+echo 1.5
+rvm use 1.9.3
 echo 2
 gem install opendelivery bundler
 echo 3
